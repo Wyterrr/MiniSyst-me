ad90466564af10d5cc933767b0d32c353408c7f1
Wyterrr <ywen998@gmail.com>
2025-05-20 17:13:20 +0200

    changement de cam

--- src/systems/init.js
+++ src/systems/init.js
@@ -2,8 +2,10 @@ import { WebGLRenderer, PerspectiveCamera, Scene } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 export default function init() {
-  const camera = new PerspectiveCamera(75, 1.77, 1, 1000);
-  camera.position.z = 8;
+  // Set a wider aspect if needed: window.innerWidth/window.innerHeight
+  const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+  // Position the camera further back and higher
+  camera.position.set(0, 10, 20);
   const renderer = new WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   const controls = new OrbitControls(camera, renderer.domElement);
